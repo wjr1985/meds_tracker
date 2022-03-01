099658f833534a74a98ca68389da04bf1f875490
Bill Rastello <wjr1985@gmail.com>
2022-03-01 07:37:44 -0600

    load dotenv earlier

--- lib/meds_tracker_google_sheets.rb
+++ lib/meds_tracker_google_sheets.rb
@@ -1,10 +1,10 @@
+require "dotenv"
+Dotenv.load
+
 require "active_support/core_ext/time"
 require "active_support/core_ext/integer/time"
-require "dotenv"
 require "google/apis/sheets_v4"
 require "googleauth"
 require "googleauth/stores/file_token_store"
-
-Dotenv.load
 
 Dir.glob(File.join(__dir__, "meds_tracker_google_sheets", "**", "*.rb"), &method(:require))
